f7c80286c3cf469272d7e287eafe78108915d901
امیر حسین فیض <amir1380feiz@gmail.com>
2023-07-13 22:28:33 +0330

    [Add] add init file :)

---
+++ train/__init__.py
@@ -0,0 +1,6 @@
+from train.train_eval import *
+from train.optimizers import *
+from train.metrics import *
+from train.model_ema import *
+from train.early_stopping import *
+from train.loss_funcs import *
