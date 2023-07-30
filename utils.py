9e103208c5010ec7cba2f1b6eb75b5f3003a6ced
امیر حسین فیض <amir1380feiz@gmail.com>
2023-07-30 22:35:08 +0330

    [Fix] fix & add existing comet experiment :)

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
+import json
 import os
 import random
 from argparse import ArgumentParser, Namespace
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Dict
 
 import comet_ml
 import numpy as np
@@ -54,9 +55,32 @@ def add_yaml_2_args_and_save_configs_and_get_device(parser: ArgumentParser,
         configs_list.append([key, value])
     configs_list.append(["device", device])
     configs["device"] = device
-    experiment = comet_ml.Experiment(log_code=True)
-    experiment.set_name(parser.parse_args().name)
-    experiment.log_parameters(configs)
+
+    if os.path.isfile(os.path.join("./train_log/", "experiment.json")):
+        with open(os.path.join("./train_log/", "experiment.json"), 'r') as openfile:
+            keys_dict = json.load(openfile)
+        if parser.parse_args().name in list(keys_dict.keys()):
+            print("Use Existing Experiment")
+            experiment = comet_ml.ExistingExperiment(previous_experiment=keys_dict[parser.parse_args().name],
+                                                     log_code=True)
+        else:
+            experiment = comet_ml.Experiment(log_code=True)
+            experiment.set_name(parser.parse_args().name)
+            experiment.log_parameters(configs)
+            keys_dict[parser.parse_args().name] = experiment.get_key()
+
+    else:
+        with open(os.path.join("./train_log/", "experiment.json"), "w") as outfile:
+            json.dump({}, outfile)
+        with open(os.path.join("./train_log/", "experiment.json"), 'r') as openfile:
+            keys_dict = json.load(openfile)
+        experiment = comet_ml.Experiment(log_code=True)
+        experiment.set_name(parser.parse_args().name)
+        experiment.log_parameters(configs)
+        keys_dict[parser.parse_args().name] = experiment.get_key()
+        with open(os.path.join("./train_log/", "experiment.json"), "w") as outfile:
+            json.dump(keys_dict, outfile)
+
     print("Load Configs")
     table = tabulate.tabulate(configs_list, headers=["name", "config"])
     print(table)
@@ -109,7 +133,7 @@ def resume(model: torch.nn.Module, optimizer: torch.optim.Optimizer, scaler: tor
     :param scheduler: learning rate scheduler
     :param model_ema: model ema
     :param args: arguments
-    :return: model, optimizer, scaler, scheduler, start_epoch, best_acc
+    :return: model, optimizer, scaler, scheduler, start_epoch, best_acc, log_dict
     """
     best_ckpt = os.path.join(args.log, f"checkpoint/best_{args.name}.pth")
     final_ckpt = os.path.join(args.log, f"checkpoint/final_{args.name}.pth")
@@ -121,6 +145,7 @@ def resume(model: torch.nn.Module, optimizer: torch.optim.Optimizer, scaler: tor
             scaler.load_state_dict(loaded["scaler"])
             scheduler.load_state_dict(loaded["scheduler"])
             start_epoch = loaded["epoch"]
+            log_dict = loaded["log_dict"]
             loaded = torch.load(best_ckpt)
             best_acc = loaded["acc"]
             if model_ema is not None:
@@ -130,15 +155,17 @@ def resume(model: torch.nn.Module, optimizer: torch.optim.Optimizer, scaler: tor
             print(f"Train start from epoch {start_epoch + 1} epoch :)")
             print(f"Best Accuracy is {best_acc} :)")
             print()
-            return model, optimizer, scaler, scheduler, start_epoch, best_acc
-        except:
-            print(f"Something is wrong! We train from Scratch :( ")
+            return model, optimizer, scaler, scheduler, start_epoch, best_acc, log_dict
+        except Exception as error:
+            print(f"Something is wrong! :( ")
+            print(error)
+            exit()
 
 
 def save(model: torch.nn.Module, acc: float, best_acc: float,
          scaler: torch.cuda.amp.GradScaler, optimizer: torch.optim.Optimizer, model_ema: Optional[torch.nn.Module],
          scheduler: torch.optim.lr_scheduler.LRScheduler, qat_model: Optional[torch.nn.Module], epoch: int,
-         args: Namespace, writer: tb.SummaryWriter, device: torch.device) -> float:
+         args: Namespace, log_dict: Dict) -> float:
     """
     save model and others
     :param model: model
@@ -151,9 +178,8 @@ def save(model: torch.nn.Module, acc: float, best_acc: float,
     :param qat_model: quantization aware training model
     :param epoch: last epoch
     :param args: arguments
+    :param log_dict: log dictionary
     :return: best mIOU
-    :param device: device cpu or cuda
-    :param writer: tensorboard
     """
     if acc > best_acc:
         print('Saving checkpoint...')
@@ -164,6 +190,7 @@ def save(model: torch.nn.Module, acc: float, best_acc: float,
             'scaler': scaler.state_dict(),
             'optimizer': optimizer.state_dict(),
             'scheduler': scheduler.state_dict(),
+            'log_dict': log_dict
         }
         if model_ema is not None:
             state.update({
