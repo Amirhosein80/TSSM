bc0ef93d68e00f93f28a474cee1ffb2f443105b7
امیر حسین فیض <amir1380feiz@gmail.com>
2023-08-19 23:24:32 +0330

    [Add] add new models to model collections :)

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,12 @@
 from models.model_utils import *
 from models.unet import *
 from models.teacher_model import *
+from models.bisenetv2 import *
+from models.regseg import *
 
 MODELS_COLLECTIONS = {
     "unet": Unet,
-    "deeplabv3": DeepLabV3
+    "deeplabv3": DeepLabV3,
+    "bisenetv2": BiSeNetV2,
+    "regseg": RegSeg,
 }
